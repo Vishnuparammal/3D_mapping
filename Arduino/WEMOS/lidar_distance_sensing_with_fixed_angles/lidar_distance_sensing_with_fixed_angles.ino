40cb5d009614938b4c65a83b5be12c09a0c96a63
Vishnuparammal <vishnu.130599@gmail.com>
2018-09-20 13:09:04 +0530

    slight mods

--- Arduino/WEMOS/lidar_distance_sensing_with_fixed_angles/lidar_distance_sensing_with_fixed_angles.ino
+++ Arduino/WEMOS/lidar_distance_sensing_with_fixed_angles/lidar_distance_sensing_with_fixed_angles.ino
@@ -4,28 +4,30 @@
 // the angle is arbitrarily chosen to check if distance is being plotted
 // data is sent in the form of string so that it can be read easily by the mapping software.
 
-#include<SoftwareSerial.h>// soft serial port header file
-SoftwareSerial Serial2(5,4); // define the soft serial port as Serial2, D1 as RX, and D2 as TX
+#include<SoftwareSerial.h>        // soft serial port header file
+SoftwareSerial Serial2(0,2);      // define the soft serial port as Serial2, D1 as RX, and D2 as TX
 /*For Arduino board with multiple serial ports such as DUE board, comment out the above two codes, and directly use Serial2 port*/
-uint16_t dist;// LiDAR actually measured distance value
-uint16_t strength;// LiDAR signal strength
-uint16_t check;// check numerical value storage
+uint16_t dist;                    // LiDAR actually measured distance value
+uint16_t strength;                // LiDAR signal strength
+uint16_t check;                   // check numerical value storage
 int i;
-byte uart[9];// store data measured by LiDAR
-byte HEADER=0x59;// data package frame header (check datasheet)
+byte uart[9];                     // store data measured by LiDAR
+byte HEADER=0x59;                 // data package frame header (check datasheet)
+
 void setup()
 {
-  Serial.begin(115200);//set the Baud rate of Arduino and computer serial port
-  Serial2.begin(115200);//set the Baud rate of LiDAR and Arduino serial port
+  Serial.begin(115200);           //set the Baud rate of Arduino and computer serial port
+  Serial2.begin(115200);          //set the Baud rate of LiDAR and Arduino serial port
 }
+
 void loop()
 {
-  if (Serial2.available())//check whether the serial port has data input
+  if (Serial2.available())        //check whether the serial port has data input
   {
-            delay(1); // this delay stabilizes code
+            delay(1);             // this delay stabilizes code
             uart[0] = Serial2.read();
             
-    if(uart[0]==HEADER)// determine data package frame header 0x59
+    if(uart[0]==HEADER)           // determine data package frame header 0x59
     {
               uart[1] = Serial2.read();
               
@@ -57,7 +59,7 @@ void loop()
           Serial.print("00");
         else if(dist<1000)
           Serial.print("0");
-        Serial.print(dist,DEC);// output LiDAR tests distance value
+        Serial.print(dist);// output LiDAR tests distance value
         Serial.print('\n');
        }
      }
